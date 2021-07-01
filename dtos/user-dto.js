c88eebec34027eaa17dee049027bac4d9e4462f6
Vlad Kondakov <vladisloove.kondakov@gmail.com>
2021-07-01 11:07:44 +0300

    delete activation code from user dto

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -8,13 +8,10 @@ module.exports = class UserDto {
 
   createdAt;
 
-  activationCode;
-
   constructor(model) {
     this.email = model.email;
     this.id = model._id;
     this.isActivated = model.isActivated;
     this.createdAt = model.createdAt;
-    this.activationCode = model.activationData.activationCode;
   }
 };
