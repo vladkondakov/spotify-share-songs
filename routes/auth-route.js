4efddfdfc1c341c8e0c96f52e6cd71c6ca5ce4af
Vlad Kondakov <vladisloove.kondakov@gmail.com>
2021-07-02 13:20:24 +0300

    fix

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -4,24 +4,24 @@ const validation = require('../middlewares/validation/validation-middleware.js')
 const authMiddleware = require('../middlewares/auth-middleware.js');
 const activationMiddleware = require('../middlewares/activation-middleware.js');
 
-router.post('/activation', userController.activateByCode);
+router.get('/activation/:code', userController.activateByCode); // post, code as a query parameter
 router.get('/activation/refresh', authMiddleware, userController.refreshActivationCode);
 router.get('/token/refresh', userController.refreshToken);
 router.post('/registration', validation.validateRegistration, userController.registration);
-router.delete('/login', validation.validateLogin, userController.login);
-router.post('/logout', userController.logout);
+router.post('/login', validation.validateLogin, userController.login);
+router.post('/logout', userController.logout); // delete
 router.get(
   '/reset/password-by-link',
   authMiddleware,
   activationMiddleware,
   userController.requestPasswordReset
 );
-router.put(
+router.post(
   '/reset/password-by-link',
   validation.validatePasswordReset,
   authMiddleware,
   activationMiddleware,
   userController.passwordResetByToken
-);
+); // put
 
 module.exports = router;
